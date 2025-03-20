6f011eb55e4390fa22516f7236f3fe46f3e6fce6
James Gebbie-Rayet <james.gebbie@stfc.ac.uk>
2025-03-20 11:53:35 +0000

    remove unused model and helpers

--- com_hecbiosim_impact/site/src/Helper/Hecbiosim_impactHelper.php
+++ com_hecbiosim_impact/site/src/Helper/Hecbiosim_impactHelper.php
@@ -12,56 +12,7 @@ defined('_JEXEC') or die;
 use \Joomla\CMS\Factory;
 use \Joomla\CMS\MVC\Model\BaseDatabaseModel;
 
-/**
- * Class Hecbiosim_impactFrontendHelper
- */
+// Class Hecbiosim_impactFrontendHelper
 class Hecbiosim_impactHelper
 {
-	
-
-	/**
-	 * Gets the files attached to an item
-	 *
-	 * @param   int     $pk     The item's id
-	 *
-	 * @param   string  $table  The table's name
-	 *
-	 * @param   string  $field  The field's name
-	 *
-	 * @return  array  The files
-	 */
-	public static function getFiles($pk, $table, $field)
-	{
-		$db = Factory::getContainer()->get('DatabaseDriver');
-		$query = $db->getQuery(true);
-
-		$query
-			->select($field)
-			->from($table)
-			->where('id = ' . (int) $pk);
-
-		$db->setQuery($query);
-
-		return explode(',', $db->loadResult());
-	}
-
-	/**
-	 * Gets the edit permission for an user
-	 *
-	 * @param   mixed  $item  The item
-	 *
-	 * @return  bool
-	 */
-	public static function canUserEdit($item)
-	{
-		$permission = false;
-		$user       = Factory::getApplication()->getIdentity();
-
-		if ($user->authorise('core.edit', 'com_hecbiosim_impact') || (isset($item->created_by) && $user->authorise('core.edit.own', 'com_hecbiosim_impact') && $item->created_by == $user->id) || $user->authorise('core.create', 'com_hecbiosim_impact'))
-		{
-			$permission = true;
-		}
-
-		return $permission;
-	}
 }
