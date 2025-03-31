9e57f53974b2f8e087f4a067293fcd395c6e6b9b
HEJ845 <164242381+HEJ845@users.noreply.github.com>
2025-03-31 09:14:00 +0100

    Update default.php

--- com_hecbiosim_impact/site/tmpl/publication/default.php
+++ com_hecbiosim_impact/site/tmpl/publication/default.php
@@ -16,6 +16,9 @@ use \Joomla\CMS\Language\Text;
 use \Joomla\CMS\Session\Session;
 use Joomla\Utilities\ArrayHelper;
 
+// Load filter data from JSON
+$filters = json_decode(file_get_contents(__DIR__ . "/pubs_config.json"), true);
+
 $params  = $this->item->params;
 ?>
 
@@ -27,40 +30,32 @@ $params  = $this->item->params;
         <h1> <?php echo $this->escape($this->params->get('page_heading')); ?> </h1>
     </div>
 <?php endif; ?>
+
 <p>A detailed list of publications that have attributed HECBioSim HPC resources or support as a significant contributor. These publications are harvested from submissions made to the EPSRC via ResearchFish.</p>
-<p></p>
+
 <div class="filters">
     <input type="text" id="searchBar" placeholder="Search publications...">
+
     <select id="typeFilter">
         <option value="All">All Types</option>
-        <option value="Journal Article">Journal Article</option>
-        <option value="Preprint">Preprint</option>
-        <option value="Conference Proceeding_Abstract">Conference Paper</option>
-        <option value="Book Chapter">Book Chapter</option>
+        <?php foreach ($filters["types"] as $type): ?>
+            <option value="<?= htmlspecialchars($type) ?>"><?= htmlspecialchars($type) ?></option>
+        <?php endforeach; ?>
     </select>
+
     <select id="dateFilter">
         <option value="All">All Years</option>
-        <option value="2025">2025</option>
-        <option value="2024">2024</option>
-        <option value="2023">2023</option>
-        <option value="2022">2022</option>
-        <option value="2021">2021</option>
-        <option value="2020">2020</option>
-        <option value="2019">2019</option>
-        <option value="2018">2018</option>
-        <option value="2017">2017</option>
-        <option value="2016">2016</option>
-        <option value="2015">2015</option>
-        <option value="2014">2014</option>
-        <option value="2013">2013</option>
+        <?php foreach ($filters["years"] as $year): ?>
+            <option value="<?= $year ?>"><?= $year ?></option>
+        <?php endforeach; ?>
     </select>
+
     <select id="projectRefFilter">
         <option value="All">All Project References</option>
-        <option value="EP/L000253/1">EP/L000253/1 (2013-2019)</option>
-        <option value="EP/R029407/1">EP/R029407/1 (2019-2023)</option>
-        <option value="EP/X035603/1">EP/X035603/1 (2023-2027)</option>
+        <?php foreach ($filters["projects"] as $key => $value): ?>
+            <option value="<?= $key ?>"><?= htmlspecialchars($value) ?></option>
+        <?php endforeach; ?>
     </select>
 </div>
 
 <div id="publications"></div>
-
